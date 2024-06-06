1cd334cf5367a18144c1e7841a3ad79ea5095aa1
GiovanniBuonsante <74987221+GiovanniBuonsante@users.noreply.github.com>
2024-06-06 15:45:24 +0200

    Delete .github/workflows directory

--- .github/workflows/blocco_richiesta.php
+++
@@ -1,4 +0,0 @@
-<div class="containerAlter">
-        <i class="fa-solid fa-ban" aria-hidden="true" style="margin: 50px; font-size: 90px; /* width: 0px; */"></i>
-        <div class="message">Bisogna aver effettuato l'accesso</div>
-</div>
